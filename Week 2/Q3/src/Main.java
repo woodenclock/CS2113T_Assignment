31e9050d958381f7fc07aa35945462fbba37824b
woodenclock <luckyisland3710@gmail.com>
2023-08-21 20:29:12 +0800

    Make Q3 1.0

--- Week 2/Q3/src/Main.java
+++ Week 2/Q3/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
-    // ADD YOUR CODE HERE
+    public static double getGradeCap(String grade) {
+        double cap = 0.0;
+        switch (grade) {
+            case "A+": cap = 5.0;
+                break;
+            case "A": cap = 5.0;
+                break;
+            case "A-": cap = 4.5;
+                break;
+            case "B+": cap = 4.0;
+                break;
+            case "B": cap = 3.5;
+                break;
+            case "B-": cap = 3.0;
+                break;
+            case "C": cap = 2.5;
+                break;
+        }
+        return cap;
+    }
 
     public static void main(String[] args) {
         System.out.println("A+: " + getGradeCap("A+"));
