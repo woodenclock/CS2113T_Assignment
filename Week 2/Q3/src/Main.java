f75f9af024e8b203a7b9038eea5bfce66a998c07
woodenclock <luckyisland3710@gmail.com>
2023-08-21 20:12:14 +0800

    Make Q3

---
+++ Week 2/Q3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    // ADD YOUR CODE HERE
+
+    public static void main(String[] args) {
+        System.out.println("A+: " + getGradeCap("A+"));
+        System.out.println("B : " + getGradeCap("B"));
+    }
+}
