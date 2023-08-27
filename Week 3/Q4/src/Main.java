a7bf5f9634c4b17779a7a9709624c6f658a6e1dd
woodenclock <luckyisland3710@gmail.com>
2023-08-27 23:10:51 +0800

    Delete previous files

--- Week 3/Q4/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Circle c = new Circle();
-
-        System.out.println(c.getArea());
-        c = new Circle(1, 2, 5);
-        System.out.println(c.getArea());
-    }
-}
