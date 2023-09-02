28858238f13815339d1b45aab05ace7bd0c6cf72
woodenclock <luckyisland3710@gmail.com>
2023-09-02 22:03:15 +0800

    Complete Week 4 Q4

--- Week 4/Q4/src/Main.java
+++ Week 4/Q4/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void describe(String color, Priority priority) {
+        System.out.println(color + " indicates " + priority.toString().toLowerCase() + " priority");
+    }
+
+    public static void main(String[] args) {
+        describe("Red", Priority.HIGH);
+        describe("Orange", Priority.MEDIUM);
+        describe("Blue", Priority.MEDIUM);
+        describe("Green", Priority.LOW);
+    }
 }
