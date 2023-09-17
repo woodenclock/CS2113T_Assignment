16803043de93388f266ac137b3fa18fad79e79c0
woodenclock <luckyisland3710@gmail.com>
2023-09-17 19:14:32 +0800

    Create Week 6 Q2

---
+++ Week 6/Q2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    private static HashMap<String, Integer> roster = new HashMap<>();
+
+    //TODO: add your methods here
+
+    public static void main(String[] args) {
+        addToRoster("Monday"); // i.e., one person signed up for Monday
+        addToRoster("Wednesday"); // i.e., one person signed up for Wednesday
+        addToRoster("Wednesday"); // i.e., another person signed up for Wednesday
+        addToRoster("Friday");
+        addToRoster("Monday");
+        printRoster();
+    }
+
+}
