638316817202ec8da451bba68e00c02f0a3164fd
woodenclock <luckyisland3710@gmail.com>
2023-09-17 19:50:05 +0800

    Update Week 6 Q2

--- Week 6/Q2/src/Main.java
+++ Week 6/Q2/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     private static HashMap<String, Integer> roster = new HashMap<>();
 
-    //TODO: add your methods here
+    private static void addToRoster(String day) {
+        if (roster.containsKey(day)){
+            Integer newValue = Integer.valueOf(roster.get(day).intValue() + 1);
+            roster.put(day, newValue);
+        } else {
+            roster.put(day, Integer.valueOf(1));
+        }
+    }
+
+    private static void printRoster() {
+        for (Map.Entry<String, Integer> signUp : roster.entrySet()) {
+            System.out.println(signUp.getKey() + " => " + signUp.getValue());
+        }
+    }
 
     public static void main(String[] args) {
         addToRoster("Monday"); // i.e., one person signed up for Monday
@@ -11,5 +27,4 @@ public class Main {
         addToRoster("Monday");
         printRoster();
     }
-
 }
